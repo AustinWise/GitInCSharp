38d036aea35876cd50e2b89a9ac5b83c149a20f7
Austin Wise <AustinWise@gmail.com>
2015-10-31 02:37:33 -0700

    Inital commit.

---
+++ GitInCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Austin.GitInCSharpLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new Repo(Environment.CurrentDirectory);
+        }
+    }
+}
---
+++ GitInCSharpLib/Repo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Austin.GitInCSharpLib
+{
+    public class Repo
+    {
+        public Repo(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException(nameof(path));
+        }
+    }
+}
