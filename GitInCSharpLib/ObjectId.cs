cb207d25352e89ab6a700ba80c296a4ef9583b4b
Austin Wise <AustinWise@gmail.com>
2015-10-31 23:26:57 -0700

    Ability to print trees.

---
+++ GitInCSharpLib/ObjectId.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Austin.GitInCSharpLib
+{
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    unsafe struct ObjectId
+    {
+        public const int SIZE = 20;
+
+        [FieldOffset(0)]
+        public fixed byte Bytes[SIZE];
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(SIZE * 2);
+            fixed   (byte* bPtr  = this.Bytes)
+            {
+                for (int i = 0; i < SIZE; i++)
+                {
+                    byte b = bPtr[i];
+                    sb.Append(b.ToString("X2", CultureInfo.InvariantCulture));
+                }
+            }
+            return sb.ToString();
+        }
+
+        public string IdStr
+        {
+            get { return ToString(); }
+        }
+
+        public static ObjectId ReadFromStream(Stream s)
+        {
+            var ret = new ObjectId();
+            for (int i = 0; i < SIZE; i++)
+            {
+                int b = s.ReadByte();
+                if (b == -1)
+                    throw new Exception("Unexpected new line.");
+                ret.Bytes[i] = (byte)b;
+            }
+            return ret;
+        }
+    }
+}
--- GitInCSharpLib/Repo.cs
+++ GitInCSharpLib/Repo.cs
@@ -1,6 +1,7 @@
 ﻿using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -48,6 +49,14 @@ namespace Austin.GitInCSharpLib
             return Encoding.ASCII.GetString(bytes.ToArray());
         }
 
+        byte[] readObjectContents(Stream s, int size)
+        {
+            byte[] buf = new byte[size];
+            if (s.Read(buf, 0, size) != size)
+                throw new Exception("Could not read it all.");
+            return buf;
+        }
+
         void inspectFile(FileInfo objectFi)
         {
             //TODO: apply some not-invented-here syndrom by reimplmenting inflate
@@ -57,9 +66,42 @@ namespace Austin.GitInCSharpLib
                 using (var inflater = new InflaterInputStream(fs))
                 {
                     string tag = readAsciiTo(inflater, ' ');
-                    string sizeStr = readAsciiTo(inflater, '\0');
-                    Console.WriteLine("{0}: {1}", tag, sizeStr);
+                    int size = int.Parse(readAsciiTo(inflater, '\0'), NumberStyles.None, CultureInfo.InvariantCulture);
+                    Console.WriteLine("{0}: {1}", tag, size);
+
+                    switch (tag)
+                    {
+                        case "blob":
+                            break;
+                        case "commit":
+                            inspectCommit(readObjectContents(inflater, size));
+                            break;
+                        case "tree":
+                            inspectTree(readObjectContents(inflater, size));
+                            break;
+                        default:
+                            throw new Exception("Unrecognized object type: " + tag);
+                    }
                 }
+            }
+        }
+
+        void inspectCommit(byte[] bytes)
+        {
+        }
+
+        unsafe void inspectTree(byte[] bytes)
+        {
+            var mem = new MemoryStream(bytes);
+            while (true)
+            {
+                int mode = Convert.ToInt32(readAsciiTo(mem, ' '), 8);
+                string fileName = readAsciiTo(mem, '\0'); //TODO: UTF-8 file names
+                ObjectId id = ObjectId.ReadFromStream(mem);
+                Console.WriteLine("\t{0,6} {1} {2}", Convert.ToString(mode, 8), id.IdStr, fileName);
+
+                if (mem.Position == bytes.Length)
+                    break;
             }
         }
     }
